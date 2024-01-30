a490336e128b59b632231964f08d7f2fe46f376e
MatviyVovchuk <doc25900@gmail.com>
2024-01-30 13:10:55 +0200

    split scss to containers & final banner layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,33 @@
+// BANNER SLIDER
+
 $(".slider-banner-content").slick({
   arrows: false,
   dots: true,
   infinity: true,
   slidesToShow: 1,
   slidesToScroll: 1,
+});
+
+// ADD SHADOW TO NAV ON
+
+$(document).ready(function() {
+  // Check scroll position on page load
+  checkScroll();
+
+  // Check scroll position on scroll
+  $(window).scroll(function() {
+    checkScroll();
+  });
+
+  function checkScroll() {
+    var navbar = $('.navbar');
+    var isTop = navbar.offset().top === 0;
+
+    // Add or remove shadow class based on scroll position
+    if (!isTop) {
+      navbar.addClass('shadow');
+    } else {
+      navbar.removeClass('shadow');
+    }
+  }
 });
